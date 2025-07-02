29642ac646e1283b96951a2536a4c085985c78da
Andres Sampayo <andresfelipesampayogeronimo@gmail.com>
2025-07-02 17:57:30 -0500

    agregar los options

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ app.mysql = mysql
 
 jwt = JWTManager(app)
 
-CORS(app)
+CORS(app,
+    origins=["http://localhost:51383", "https://biblioteca-v97t.onrender.com"],
+    supports_credentials=True,
+    allow_headers=["Content-Type", "Authorization"],
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
 registrar_rutas(app)
 
